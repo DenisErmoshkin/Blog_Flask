ddca46f246ae060a9a95ff0cfc21c2f42ab9b854
dener1974 <dener1974@icloud.com>
2023-04-20 19:23:51 +0200

    qwe

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
-def index ():
-    return "Hello, World! <br> and FACK U BEACH!"
+def index():
+    user = {'username': 'DENIS'}
+    return render_template('index.html', title='Home', user=user)
 
 @app.route('/blog')
 def blog ():
-    return "HELLO THIS IS MY BLOG, NICE TO MEET U <br> and HAVE A GOOD DAY, BEACHES!"
+    return "HELLO THIS IS MY BLOG, NICE TO MEET U <br> and HAVE A GOOD DAY, BITCHES!"
+
+@app.route('/about')
+def about ():
+    return "HELLO THIS IS SECTION ABOUT, NICE TO MEET U <br> and HAVE A GOOD DAY, BITCHES!"
