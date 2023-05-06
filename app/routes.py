b2ae2cf960aa259591874952d04c9fe94f2e7015
dener1974 <dener1974@icloud.com>
2023-05-06 18:29:48 +0200

    qwe

--- app/routes.py
+++ app/routes.py
@@ -31,6 +31,7 @@ def login ():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}' .format(form.username.data, form.remember_me.data))
         return redirect('/index')
+        #...#
     return render_template('login.html', form=form)
 
 
