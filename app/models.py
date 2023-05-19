3c85c0453b6048e40f1fee2d7b6737e283c3219f
dener1974 <dener1974@icloud.com>
2023-05-19 20:40:32 +0200

    qweqwe

--- app/models.py
+++ app/models.py
@@ -13,12 +13,16 @@ def load_user(id):
 
 class User(UserMixin, db.Model):  # класс User - модель базы данных. Он наследуется от db.Model, и использует
     # ORM (объектно-реляционное отображение) для взаимодействия с базой данных.
+    # автоматизирует перенос данных, хранящихся в таблицах реляционной базы данных
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
-    def __repr__(self):
+    def __repr__(self):  #Метод __repr__ в Python является специальным методом, который используется для определения
+        # строкового представления объекта. Когда вызывается функция repr() для объекта, она возвращает строку,
+        # которая представляет объект таким образом, что эта строка может быть использована для создания эквивалентного
+        # объекта с помощью функции eval() или при вводе в интерактивной оболочке.
         return '<User {}>'.format(self.username)
 
     def set_password(self, password):
