a37f53563c822c5627ef8042d2c72c5f3b380be0
dener1974 <dener1974@icloud.com>
2023-05-30 17:52:12 +0200

    qweqwe

--- app/routes.py
+++ app/routes.py
@@ -72,6 +72,8 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = [
         {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
+        {'author': user, 'body': 'Test post #2'},
+        {'author': user, 'body': 'Test post #3'},
+        {'author': user, 'body': 'Test post #4'}
     ]
     return render_template('user.html', user=user, posts=posts)
